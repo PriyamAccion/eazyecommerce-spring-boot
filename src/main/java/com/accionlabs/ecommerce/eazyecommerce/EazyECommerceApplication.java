3a736e98c107f8d31d410c1fb19dd52e7865680d
PriyamAccion <agarwal.p7997@gmail.com>
2026-02-16 19:03:59 +0530

    Initial setup

---
+++ src/main/java/com/accionlabs/ecommerce/eazyecommerce/EazyECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.accionlabs.ecommerce.eazyecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyECommerceApplication.class, args);
+    }
+
+}
